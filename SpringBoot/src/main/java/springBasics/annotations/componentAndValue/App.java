81b1832b3474ffcb8d41cb63775785960fb31aa1
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-12 20:20:01 +0530

    Component scan

---
+++ SpringBoot/src/main/java/springBasics/annotations/componentAndValue/App.java
@@ -0,0 +1,14 @@
+package springBasics.annotations.componentAndValue;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new ClassPathXmlApplicationContext("applicationComponentAndValue.xml");
+
+        Employee employee = context.getBean("employee", Employee.class);
+        System.out.println(employee.toString());
+    }
+}
