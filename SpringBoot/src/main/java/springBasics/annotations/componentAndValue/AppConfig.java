eb471786f2cba8b2405026009ff56090b1b3242d
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-12 20:27:26 +0530

    Component and Value annotation

---
+++ SpringBoot/src/main/java/springBasics/annotations/componentAndValue/AppConfig.java
@@ -0,0 +1,10 @@
+package springBasics.annotations.componentAndValue;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "springBasics.annotations.componentAndValue")
+public class AppConfig {
+
+}
