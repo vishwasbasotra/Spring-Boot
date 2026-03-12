4281ceb23d6c9e6414a42e3892750b55dd5a8db8
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-12 20:51:46 +0530

    @Qualifier

---
+++ SpringBoot/src/main/java/springBasics/annotations/autowired/AppConfig.java
@@ -0,0 +1,10 @@
+package springBasics.annotations.autowired;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "springBasics.annotations.autowired")
+public class AppConfig {
+
+}
---
+++ SpringBoot/src/main/java/springBasics/annotations/autowired/Manager.java
@@ -0,0 +1,24 @@
+package springBasics.annotations.autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Manager {
+    @Autowired  //field injection
+    @Qualifier("employee")
+    Employee employee;
+
+//    @Autowired    //constructor injection
+//    public Manager(Employee employee) {
+//        this.employee = employee;
+//    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}
