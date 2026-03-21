9b8286572e0bfd4a64f429f1e86f5e15c0e49d31
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-21 21:55:29 +0530

    Creating category controller

---
+++ ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,25 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+    private List<Category> categoryList = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories(){
+        return categoryList;
+    }
+
+    @PostMapping("/api/admin/categories")
+    public String createCategory(@RequestBody String category){
+        return "New Category Created: "+ category;
+    }
+}
