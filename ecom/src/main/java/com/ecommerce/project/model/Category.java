ab98283235b897859562da46b12f12c1dc69d02a
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 22:11:22 +0530

    added automated category ids

--- ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -2,22 +2,23 @@ package com.ecommerce.project.model;
 
 
 public class Category {
-    private Long categoryId;
+    private static long categoryCount = 1;
+    private String categoryId;
     private String categoryName;
 
     public Category() {
     }
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
+    public Category(String categoryName) {
+        this.categoryId = "Category-"+categoryCount++;
         this.categoryName = categoryName;
     }
 
-    public Long getCategoryId() {
+    public String getCategoryId() {
         return categoryId;
     }
 
-    public void setCategoryId(Long categoryId) {
+    public void setCategoryId(String categoryId) {
         this.categoryId = categoryId;
     }
 
