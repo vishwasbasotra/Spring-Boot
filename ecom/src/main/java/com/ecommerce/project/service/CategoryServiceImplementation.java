cb2e1fc4d27d96c6971f39f8cf566d9c01caa152
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 19:43:51 +0530

    Create CategoryServiceImplementation.java

---
+++ ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImplementation.java
@@ -0,0 +1,22 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CategoryServiceImplementation implements CategoryService{
+    private List<Category> categoryList = new ArrayList<>();
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryList;
+    }
+
+    @Override
+    public void createNewCategory(Category category) {
+        categoryList.add(category);
+    }
+}
