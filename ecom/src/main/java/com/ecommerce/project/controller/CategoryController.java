8c0067019e1ca9dac0a8dafcd74e7a2e713d56b1
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-25 22:10:47 +0530

    added delete functionality

--- ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,26 +1,35 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoryController {
-    private List<Category> categoryList = new ArrayList<>();
+
+    @Autowired
+    private CategoryService categoryService;
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
-        return categoryList;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/admin/categories")
     public String createCategory(@RequestBody Category category){
-        categoryList.add(category);
+        categoryService.createNewCategory(category);
         return "New Category Created Successfully";
+    }
+
+    @PostMapping("/api/admin/categories")
+    public String deleteCategory(@RequestBody String categoryId){
+        categoryService.deleteCategory(categoryId);
+        return "Category Deleted";
     }
 }
