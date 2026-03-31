4e896acaff207f775515431d33ee6cfd24fce3fa
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-03-31 19:27:49 +0530

    Update Category.java

--- ecom/src/main/java/com/ecommerce/project/entity/Category.java
+++ ecom/src/main/java/com/ecommerce/project/entity/Category.java
@@ -17,8 +17,7 @@ public class Category {
     public Category() {
     }
 
-    public Category(Long categoryId, String categoryName) {
-        this.categoryId = categoryId;
+    public Category(String categoryName) {
         this.categoryName = categoryName;
     }
 
