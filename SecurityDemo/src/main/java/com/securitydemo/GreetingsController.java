67164e0e4db049d19cf2922ff29dc6b3439cc529
Vishwas Basotra <brigu.sharma.9@gmail.com>
2026-04-14 21:50:56 +0530

    Implementing spring security in the project

---
+++ SecurityDemo/src/main/java/com/securitydemo/GreetingsController.java
@@ -0,0 +1,14 @@
+package com.securitydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
+}
